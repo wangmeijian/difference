70d19d904e51791a045e74ff0592532477d36a66
wangmeijian <wangmeijian@qianxin.com>
2020-07-05 17:21:40 +0800

    解决日志生成bug

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # difference
 
-## 对比js数据修改前后差异，生成操作日志
-
+### 对比js数据修改前后差异，生成操作日志
 
 ```js
-import difference from 'difference';
+import difference from './difference'
 
 // 数据修改前  
 let before = {
-  name: 'undefined',
-  age: '18',
-  sex: '1',
+  name: 'wang',
+  sex: 1,
   skill: ['javascript', 'html', 'css'],
   asset: {
-    house: 10,
+    house: 5,
     car: 6,
     dog: 1
   }
@@ -21,16 +19,114 @@ let before = {
 
 // 数据修改后  
 let after = {
-  name: 'undefined',
-  age: '20',
-  sex: '2',
+  name: 'wang',
+  sex: 2,
   skill: ['javascript', 'html', 'vue', 'react'],
   asset: {
-    house: 18,
+    house: 10,
     car: 6,
     cat: 2
-  }
+  },
+  job: null
 };
 
 const diff = difference(after, before);
 ```
+输出结果为如下格式的数组：
+
+```js
+[{
+  // 字段路径
+  path: [],
+  // 字段描述
+  path_desc: [],
+  // 操作共3种：ADD、REMOVE、UPDATE，即增、删、改
+  action: "ADD",
+  // 修改前的值
+  modify_from: "",
+  // 修改后的值
+  modify_to: ""
+}]
+```
+
+结果用表格展示如下：
+
+| 操作 | 字段路径 | 修改前 | 修改后 |
+| -- | -- | -- | -- |
+| UPDATE | sex | 1 | 2 |
+| UPDATE | skill/2 | css | vue |
+| ADD | skill/3 |  | react |
+| UPDATE | stock/0/apple | 100 | 500 |
+| UPDATE | asset/house | 5 | 10 |
+| ADD | asset/cat |  | 2 |
+| ADD | job |  | null |
+| REMOVE | asset/dog | 1 |  |
+
+配合字典增强可读性
+
+```js
+// 字典
+let dict = {
+  name: {
+    _label: '姓名'
+  },
+  age: {
+    _label: '年龄'
+  },
+  sex: {
+    _label: '性别',
+    // 枚举数据
+    _enum: {
+      1: '男',
+      2: '女'
+    }
+  },
+  // 元素为基本数据类型的数组
+  skill: {
+    _label: '技能'
+  },
+  // 元素为对象的数组
+  stock: {
+    _label: '股票',
+    apple: {
+      _label: '苹果'
+    },
+    google: {
+      _label: '谷歌'
+    }
+  },
+  asset: {
+    _label: '资产',
+    house: {
+      _label: '房子'
+    },
+    car: {
+      _label: '豪车'
+    },
+    cat: {
+      _label: '猫'
+    },
+    dog: {
+      _label: '狗'
+    }
+  },
+  job: {
+    _label: '工作'
+  }
+}
+
+const diff = difference(after, before, dict);
+```
+输出结果为数组，用表格展示如下：
+
+| 操作 | 字段描述 | 修改前 | 修改后 |
+| -- | -- | -- | -- |
+| UPDATE | 性别 | 男 | 女 |
+| UPDATE | 技能 | css | vue |
+| ADD | 技能 |  | react |
+| UPDATE | 资产/房子 | 5 | 10 |
+| UPDATE | 股票/苹果 | 100 | 500 |
+| ADD | 	资产/猫 |  | 2 |
+| ADD | 工作 |  | null |
+| REMOVE | 	资产/狗 | 1 |  |
+
--- index.js
+++ index.js
@@ -21,16 +21,25 @@ function modify(newData = {}, oldData = {}, dictionaries = {}, path = []) {
       diff.push(...modify(newVal, oldVal, dictionaries, [...path, key]));
     } else if (newVal !== oldVal) {
       const action = typeof oldVal === "undefined" ? "ADD" : "UPDATE";
-      const { desc, enumerable } = createPathDesc(dictionaries, [...path, key]);
 
-      diff.push({
-        path: [...path, key],
-        path_desc: desc,
-        action,
-        modify_from: action === "ADD" ? "" : enum2Text(enumerable, oldVal),
-        modify_to: enum2Text(enumerable, newVal),
-      });
-      delete oldData[key];
+      if (newVal && typeof newVal === "object") {
+        diff.push(...modify(newVal, {}, dictionaries, [...path, key]));
+      } else {
+        console.log([...path, key]);
+        const { desc, enumerable } = createPathDesc(dictionaries, [
+          ...path,
+          key,
+        ]);
+
+        diff.push({
+          path: [...path, key],
+          path_desc: desc,
+          action,
+          modify_from: action === "ADD" ? "" : enum2Text(enumerable, oldVal),
+          modify_to: enum2Text(enumerable, newVal),
+        });
+        delete oldData[key];
+      }
     } else if (newVal === oldVal) {
       delete oldData[key];
     }
@@ -71,43 +80,53 @@ function remove(newData = {}, oldData = {}, dictionaries = {}, path = []) {
  * @returns {array[string]}
  */
 function createPathDesc(dictionaries = {}, path = []) {
-  if (Object.keys(dictionaries).length === 0 || path.length === 0) return "";
+  if (Object.keys(dictionaries).length === 0 || path.length === 0) {
+    return {
+      desc: [],
+      enumerable: {},
+    };
+  }
+
   let desc = [];
   let dict = dictionaries;
 
   for (let i = 0; i < path.length; i++) {
-    if (dict._array) {
-      dict = dict._array;
-    } else {
-      dict = dict[path[i]] || {};
-    }
-    if (dict && dict.label) {
-      desc.push(dict.label);
+    if (dict._array || dict[path[i]]) {
+      dict = dict._array || dict[path[i]];
+      dict && dict._label && desc.push(dict._label);
     }
   }
   return {
     desc,
-    enumerable: dict._enum || null,
+    enumerable: dict._enum || {},
   };
 }
 /**
  * 枚举数据转换为文本
- * @param {object} enums
+ * @param {object} enumerable 枚举数据对象
  * @param {string|number|boolean} value
  */
 function enum2Text(enumerable = {}, value) {
-  return enumerable && enumerable[value] ? enumerable[value] : value;
+  return enumerable && typeof enumerable[value] !== "undefined"
+    ? transformNull(enumerable[value])
+    : transformNull(value);
 }
 
-const difference = (
-  newData = {},
-  oldData = {},
-  dictionaries = {},
-  path = []
-) => {
+function transformNull(value) {
+  return value === null ? "null" : value;
+}
+
+/**
+ * 数据操作日志
+ * @param {object} newData 修改后的数据
+ * @param {object} oldData 修改前的数据
+ * @param {object} dictionaries 字典描述
+ * @returns {array} 操作日志
+ */
+const difference = (newData = {}, oldData = {}, dictionaries = {}) => {
   return [
-    ...modify(newData, oldData, dictionaries, path),
-    ...remove(newData, oldData, dictionaries, path),
+    ...modify(newData, oldData, dictionaries),
+    ...remove(newData, oldData, dictionaries),
   ];
 };
 
